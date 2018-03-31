2c0f0441ea67d11f59c38396d7873db3c51351ea
vigneshwar-balakrishnan <vigneshwar.balakrishnan@gmail.com>
2018-03-31 23:50:09 +0530

    destructured props and added scroll view

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,17 +3,26 @@ import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetails = (props) => {
+const AlbumDetails = ({album}) => {
+    const {title, artist, thumbnail_image, image  } = album;
 return (
     <Card>
         <CardSection>
-            <View>
-                <Image source={{uri: props.album.thumbnail_image}}/>
+            <View style= {styles.thumbnailContainerStyle}>
+                <Image style={styles.thumbnailStyle} 
+                source={{uri: thumbnail_image}}
+                />
             </View>
             <View style={styles.headerContentStyle}>
-              <Text>{props.album.title}</Text>
-              <Text>{props.album.artist}</Text>
+              <Text style={styles.headerTextStyle}>{title}</Text>
+              <Text>{artist}</Text>
             </View>
+        </CardSection>
+        <CardSection> 
+            <Image
+             style={styles.imageStyle}
+             source={{ uri: image }} 
+             />
         </CardSection>
     </Card>
 )
@@ -23,6 +32,25 @@ const styles = {
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18,
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 }
 
